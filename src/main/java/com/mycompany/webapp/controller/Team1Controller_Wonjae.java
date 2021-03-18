3b691bb842b420e1624c1386d6f7bd17ff0dad2a
OhWonjae <dhdnjswo5000@naver.com>
2021-03-19 01:57:36 +0900

    wonjae commit - TempController

---
+++ src/main/java/com/mycompany/webapp/controller/Team1Controller_Wonjae.java
@@ -0,0 +1,31 @@
+package com.mycompany.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/index")
+public class Team1Controller_Wonjae {
+	@GetMapping("/search")
+	public String rank() {
+		return "/index/search";
+	}
+	
+	@GetMapping("/payFinish")
+	public String rec() {
+		return "/index/payFinish";
+	}
+	
+
+	
+	@GetMapping("/detail")
+	public String login() {
+		return "/index/detail";
+	}
+	
+	@GetMapping("/pay")
+	public String signUp() {
+		return "/index/pay";
+	}
+}
