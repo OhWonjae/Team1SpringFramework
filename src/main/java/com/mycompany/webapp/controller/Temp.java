a55b75ea0ca3b73e55485bafeea3f061ca1ba7a3
OhWonjae <dhdnjswo5000@naver.com>
2021-03-18 20:57:20 +0900

    cc

--- src/main/java/com/mycompany/webapp/controller/Temp.java
+++ src/main/java/com/mycompany/webapp/controller/Temp.java
@@ -1,5 +1,13 @@
 package com.mycompany.webapp.controller;
 
-public class Temp {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class Temp {
+	@RequestMapping("/")
+	public String Test() {
+		System.out.println("dsaf");
+		return "detail";
+	}
 }
