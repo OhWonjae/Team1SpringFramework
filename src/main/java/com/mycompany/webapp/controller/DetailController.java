13d9ad23e6eb3230d6929ab3a712271ee1386f0b
OhWonjae <dhdnjswo5000@naver.com>
2021-03-20 00:52:28 +0900

    controller경로, index.jsp수정

--- src/main/java/com/mycompany/webapp/controller/DetailController.java
+++ src/main/java/com/mycompany/webapp/controller/DetailController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/home")
+@RequestMapping("/index")
 public class DetailController {
 
 	//제품 상세페이지 이동
