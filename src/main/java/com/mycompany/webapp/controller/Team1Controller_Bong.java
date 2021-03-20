53e772dee0089005ac6039a3b58e26d920e1270a
MSI <MSI@DESKTOP-A40NC9U>
2021-03-20 23:25:15 +0900

    bong

--- src/main/java/com/mycompany/webapp/controller/Team1Controller_Bong.java
+++ src/main/java/com/mycompany/webapp/controller/Team1Controller_Bong.java
@@ -8,22 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/index")
 public class Team1Controller_Bong {
-	@GetMapping("/new")
-	public String newMethod() {
-		return "/index/new";
-	}
-	@GetMapping("/phoneChange")
-	public String phoneChange() {
-		return "/index/phoneChange";
-	}
-	@GetMapping("/pwChange")
-	public String pwChange() {
-		return "/index/pwChange";
-	}
-	@GetMapping("/my")
-	public String my() {
-		return "/index/my";
-	}
 	@GetMapping("/rec")
 	   public String rec() {
 	      return "/index/rec";
