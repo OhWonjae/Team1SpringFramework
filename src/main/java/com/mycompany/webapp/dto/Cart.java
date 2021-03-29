859efe180d8a0d16a4a988ce605a64678f15719f
kimhyeongyun <ske011217@gmail.com>
2021-03-29 09:28:22 +0900

    형윤 수정

--- src/main/java/com/mycompany/webapp/dto/Cart.java
+++ src/main/java/com/mycompany/webapp/dto/Cart.java
@@ -1,43 +1,43 @@
 package com.mycompany.webapp.dto;
 
 
-public class Cart {	
-	private int cart_id;
-	private String cart_amount;
-	private Product product;
-	private User user;
-	private Photo photo;
-	
-	
-	public int getCart_id() {
-		return cart_id;
-	}
-	public void setCart_id(int cart_id) {
-		this.cart_id = cart_id;
-	}
-	public String getCart_amount() {
-		return cart_amount;
-	}
-	public void setCart_amount(String cart_amount) {
-		this.cart_amount = cart_amount;
-	}
-	public Product getProduct() {
-		return product;
-	}
-	public void setProduct(Product product) {
-		this.product = product;
-	}
-	public User getUser() {
-		return user;
-	}
-	public void setUser(User user) {
-		this.user = user;
-	}
-	public Photo getPhoto() {
-		return photo;
-	}
-	public void setPhoto(Photo photo) {
-		this.photo = photo;
-	}	
-}
+public class Cart {   
+   private int cid;
+   private String camount;
+   private Product product;
+   private User user;
+   private Photo photo;
+   public int getCid() {
+      return cid;
+   }	
+   public void setCid(int cid) {
+      this.cid = cid;
+   }
+   public String getCamount() {
+      return camount;
+   }
+   public void setCamount(String camount) {
+      this.camount = camount;
+   }
+   public Product getProduct() {
+      return product;
+   }
+   public void setProduct(Product product) {
+      this.product = product;
+   }
+   public User getUser() {
+      return user;
+   }
+   public void setUser(User user) {
+      this.user = user;
+   }
+   public Photo getPhoto() {
+      return photo;
+   }
+   public void setPhoto(Photo photo) {
+      this.photo = photo;
+   }
+   
+   
 
+}
