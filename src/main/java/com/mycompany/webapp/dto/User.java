47098a8e644dc9d51c8e7aa0b55eb17a2875e3bf
kimhyeongyun <ske011217@gmail.com>
2021-03-31 18:29:24 +0900

    형윤 커밋

--- src/main/java/com/mycompany/webapp/dto/User.java
+++ src/main/java/com/mycompany/webapp/dto/User.java
@@ -1,65 +1,28 @@
 package com.mycompany.webapp.dto;
 
 public class User {
-	private int uid;
-	private String uname;
-	private String uemail;
-	private String upassword;
-	private String uphone;
-	private String dogsize;
-	private int uenabled;
-	private String uauthority;
-	
-	
-	
-	public int getUid() {
-		return uid;
-	}
-	public void setUid(int uid) {
-		this.uid = uid;
-	}
-	public String getUname() {
-		return uname;
-	}
-	public void setUname(String uname) {
-		this.uname = uname;
-	}
-	public String getUemail() {
-		return uemail;
-	}
-	public void setUemail(String uemail) {
-		this.uemail = uemail;
-	}
-	public String getUpassword() {
-		return upassword;
-	}
-	public void setUpassword(String upassword) {
-		this.upassword = upassword;
-	}
-	public String getUphone() {
-		return uphone;
-	}
-	public void setUphone(String uphone) {
-		this.uphone = uphone;
-	}
-
-	
-	public String getDogsize() {
-		return dogsize;
-	}
-	public void setDogsize(String dogsize) {
-		this.dogsize = dogsize;
-	}
-	public int getUenabled() {
-		return uenabled;
-	}
-	public void setUenabled(int uenabled) {
-		this.uenabled = uenabled;
-	}
-	public String getUauthority() {
-		return uauthority;
-	}
-	public void setUauthority(String uauthority) {
-		this.uauthority = uauthority;
-	}
+	/*
+	 * private int uid; private String uname; private String uemail; private String
+	 * upassword; private String uphone; private String dogsize; private int
+	 * uenabled; private String uauthority;
+	 * 
+	 * 
+	 * 
+	 * public int getUid() { return uid; } public void setUid(int uid) {
+	 * 
+	 * this.uid = uid; } public String getUname() { return uname; } public void
+	 * setUname(String uname) { this.uname = uname; } public String getUemail() {
+	 * return uemail; } public void setUemail(String uemail) { this.uemail = uemail;
+	 * } public String getUpassword() { return upassword; } public void
+	 * setUpassword(String upassword) { this.upassword = upassword; } public String
+	 * getUphone() { return uphone; } public void setUphone(String uphone) {
+	 * this.uphone = uphone; }
+	 * 
+	 * 
+	 * public String getDogsize() { return dogsize; } public void setDogsize(String
+	 * dogsize) { this.dogsize = dogsize; } public int getUenabled() { return
+	 * uenabled; } public void setUenabled(int uenabled) { this.uenabled = uenabled;
+	 * } public String getUauthority() { return uauthority; } public void
+	 * setUauthority(String uauthority) { this.uauthority = uauthority; }
+	 */
 }
