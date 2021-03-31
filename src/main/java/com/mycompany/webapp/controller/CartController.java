deb5ef8a82622dafd7bd7912a426daddc888ace3
jeongyewon <yeewonee@naver.com>
2021-03-31 14:45:46 +0900

    예원 커밋

--- src/main/java/com/mycompany/webapp/controller/CartController.java
+++ src/main/java/com/mycompany/webapp/controller/CartController.java
@@ -1,20 +1,69 @@
 package com.mycompany.webapp.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.mycompany.webapp.dto.Cart;
+import com.mycompany.webapp.service.CartsService;
+
 @Controller
 @RequestMapping("/order")
 public class CartController {
-
+	@Autowired
+	   private CartsService cartsService; 
+	
 	@GetMapping("/cart")
-	public String cart() {
+	public String cart(Model model) {
+		
+		List<Cart> list = cartsService.getCartList();
+		model.addAttribute("list", list);
+		
 		return "/order/cart";
 	}
+	
 	@GetMapping("/putcart")
 	public String putcart() {
 		return "redirect:/product/detail";
 	}
+	
+	@GetMapping("/cart/increase")
+	public String increase(int uid, int pid) {
+		cartsService.plusAmount(uid, pid);
+		return "redirect:/order/cart";
+	}
+	
+	@GetMapping("/cart/decrease")
+	public String decrease(int uid, int pid) {
+		List<Cart> list = cartsService.getCartList();
+		for(int i=0; i<list.size(); i++) {
+			if(list.get(i).getUid() == uid
+					&& list.get(i).getPid() == pid) {
+				if(Integer.parseInt(list.get(i).getCamount()) > 1) {
+					cartsService.minusAmount(uid, pid);	
+					break;
+				}
+			}
+		}
+		return "redirect:/order/cart";
+	}
+	
+	@GetMapping("/cart/delete")
+	public String delete(int uid, int pid) {
+		cartsService.removeCartList(uid, pid);
+		return "redirect:/order/cart";
+	}
+	
+	@GetMapping("/cart/deleteall")
+	public String deleteAll(int uid) {
+		cartsService.removeCartAll(uid);
+		return "redirect:/order/cart";
+	}
 }
+
+
---
+++ src/main/java/com/mycompany/webapp/service/CartsService.java
@@ -0,0 +1,38 @@
+package com.mycompany.webapp.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mycompany.webapp.dao.CartDao;
+import com.mycompany.webapp.dto.Cart;
+
+@Service
+public class CartsService {
+
+	@Autowired
+	private CartDao cartDao;
+	
+	public List<Cart> getCartList(){ 
+		List<Cart> list = cartDao.cartList(20);
+		return list;
+	}
+	
+	public void plusAmount(int uid, int pid) {
+		cartDao.increaseAmount(uid, pid);
+	}
+	
+	public void minusAmount(int uid, int pid) {
+		cartDao.decreaseAmount(uid, pid);
+	}
+	
+	public void removeCartList(int uid, int pid) {
+		cartDao.deleteCart(uid, pid);
+	}
+	
+	public void removeCartAll(int uid) {
+		cartDao.deleteCartAll(uid);
+	}
+
+}
