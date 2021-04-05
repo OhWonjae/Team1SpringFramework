b9fd8ee0b0c8c5e0825248d5705f2d534eed144e
kimhyeongyun <ske011217@gmail.com>
2021-04-06 00:27:27 +0900

    형윤 오늘한부분 푸쉬(회원가입 오류 수정)

--- src/main/java/com/mycompany/webapp/dao/UsersDao.java
+++ src/main/java/com/mycompany/webapp/dao/UsersDao.java
@@ -8,25 +8,20 @@ import com.mycompany.webapp.dto.User;
 @Mapper
 
 public interface UsersDao {
-	
-	
-	public int insert(User user);
 
-	
+	public int insert(User user);
 
 	public User selectIdByNameAndPassword(@Param("user_name") String user_name, @Param("user_phone") String user_phone);
 
-	//public int updatePasswordById(@Param("user_password") String user_password, @Param("user_id") String user_id);
+	// public int updatePasswordById(@Param("user_password") String user_password,
+	// @Param("user_id") String user_id);
 
 	public int updatePhoneById(@Param("user_phone") String user_phone, @Param("user_id") String user_id);
-
 
 	public int updatePasswordById(@Param("user_password") String user_password, @Param("user_id") String user_id);
 
 	public User selectByUserid(String user_id);
-	
-	public int idCheck(User user) throws Exception;
-	
-	
+
+	public int idCheck(String user_id);
 
 }
